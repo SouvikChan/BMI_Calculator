67df85d3de34fca3ddfd37fb3f8f58687f48488e
Souvik <souvikchandra9454@gmail.com>
2022-06-07 13:24:26 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    height: 0,
+    mass: 0,
+    resultNumber: 0,
+    resultText: "",
+  };
+
+  handleCalculate = () => {
+    let imc = Number((this.state.mass/Math.pow(this.state.height,2)) * 10000);
+    this.setState({
+      resultNumber: imc.toFixed(1),
+    });
+
+    if (imc < 18.5) {
+      this.setState({ resultText: "Underweight" });
+    } else if (imc > 18.5 && imc < 25) {
+      this.setState({ resultText: "Normal Weight" });
+    } else if (imc >= 25 && imc < 30) {
+      this.setState({ resultText: "Overweight" });
+    } else {
+      this.setState({ resultText: "Obesity" });
+    }
+  };
+
+  render() {
+    return (
+      <ImageBackground
+        source={require("./components/images/bg.png")}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <View style={styles.container}>
+          <Text
+            style={{
+              color: "#FFCB1F",
+              justifyContent: "center",
+              alignSelf: "center",
+              marginTop: 30,
+              fontSize: 15,
+            }}
+          >
+            BMI Calculator
+          </Text>
+          <View style={styles.intro}>
+            <TextInput
+              placeholder="Height"
+              keyboardType="numeric"
+              style={styles.input}
+              onChangeText={(height) => {
+                this.setState({ height });
+              }}
+            />
+            <TextInput
+              placeholder="Mass"
+              keyboardType="numeric"
+              style={styles.input}
+              onChangeText={(mass) => {
+                this.setState({ mass });
+              }}
+            />
+          </View>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={this.handleCalculate}
+          >
+            <Text style={styles.buttonText}>Calculate </Text>
+          </TouchableOpacity>
+          <Text style={styles.result}>{this.state.resultNumber}</Text>
+          <Text style={[styles.result, { fontSize: 35 }]}>
+            {this.state.resultText}
+          </Text>
+        </View>
+      </ImageBackground>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  intro: {
+    flexDirection: "row",
+  },
+  input: {
+    height: 80,
+    textAlign: "center",
+    width: "50%",
+    fontSize: 50,
+    marginTop: 24,
+    color: "#FFCB1F",
+  },
+  button: {
+    backgroundColor: "#1D1D1B",
+  },
+  buttonText: {
+    alignSelf: "center",
+    padding: 30,
+    fontSize: 25,
+    color: "#FFCB1F",
+    fontWeight: "bold",
+  },
+  result: {
+    alignSelf: "center",
+    color: "#FFCB1F",
+    fontSize: 65,
+    padding: 15,
   },
 });
